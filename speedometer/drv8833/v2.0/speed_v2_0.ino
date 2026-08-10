18d615e36ef1fe320e165dc0ff3c62e3c972e821
doroXer <69560410+doroXer@users.noreply.github.com>
2026-08-11 01:09:20 +0900

    Add speedometer v2.0 entry sketch / スピードメーターv2.0を追加

---
+++ speedometer/drv8833/v2.0/speed_v2_0.ino
@@ -0,0 +1,11 @@
+// Honda Beat Speedometer v2.0
+// Source logic is assembled from the following include parts.
+// ホンダ・ビート スピードメーター v2.0
+// 下記includeファイルを展開するとv2.0の実装になります。
+
+#include "source_part01.inc"
+#include "source_part02.inc"
+#include "source_part03.inc"
+#include "source_part04.inc"
+#include "source_part05.inc"
+#include "source_part06.inc"
